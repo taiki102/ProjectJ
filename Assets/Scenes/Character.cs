f5bf8aae32e28b268ff1b28b4ea1c61c6211b7c7
taiki102 <raita01111@gmail.com>
2024-05-29 14:41:04 +0900

    Add BillBreak

--- Assets/Scenes/Character.cs
+++ Assets/Scenes/Character.cs
@@ -5,58 +5,86 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     float count = 0f;
+    bool IsMoving = true;
     bool IsAttack = false;
     float AttackCount = 0f;
-    float speed = 50.0f;
+    float speed = 40.0f;
 
     GameObject TargetObj;
 
     [SerializeField]
     Animator Animator;
 
+    [SerializeField]
+    Transform PunchPos;
+
     void Update()
     {
         if (IsAttack)
         {
             AttackCount += Time.deltaTime;
-            if (AttackCount > 1.0f)
+            if (AttackCount > 1.5f)
             {
                 AttackCount = 0f;
                 IsAttack = false;
+                IsMoving = true;
                 Animator.SetBool("Punch", false);
 
-                GameManager.instance.DestroyBill();
+
+                RaycastHit hit = new RaycastHit();
+                hit.point = PunchPos.position;
+                TargetObj.GetComponent<SimplestarGame.VoronoiFragmenter>().Fragment(hit);
+
+                GameManager.instance.DestroyBill(TargetObj);
+                TargetObj = null;
             }
         }
-        else
+        else if(IsMoving && TargetObj != null)
+        {
+            Vector3 pos = transform.position;
+            Vector3 direction = (TargetObj.transform.position - pos).normalized;
+            //Debug.Log(TargetObj.transform.position + "" + (TargetObj.transform.position - pos));
+
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            targetRotation.x = 0f;
+            targetRotation.z = 0f;
+            transform.rotation = targetRotation;
+
+            Vector3 movement = transform.forward * speed * Time.deltaTime;
+            movement.y = 0f;
+            transform.position = pos + movement;
+
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, 40.0f);
+            foreach (var hitCollider in hitColliders)
+            {
+                if (hitCollider.gameObject == TargetObj)
+                {
+                    Debug.Log("Get");
+
+                    Animator.SetBool("Punch", true);
+                    Quaternion rotation = transform.rotation;
+                    rotation.y += 0.4f;
+                    transform.rotation = rotation;
+
+                    count = 0;
+
+                    IsMoving = false;
+                    IsAttack = true;
+                    break; 
+                }
+            }
+
+        }else
         {
             count += Time.deltaTime;
             if (count > 1.0f)
             {
                 count = 0;
                 TargetObj = GameManager.instance.GetBillObj();
-            }
-        }
 
-        if (TargetObj != null)
-        {
-            Vector3 pos = transform.position;
-            Vector3 plus = (TargetObj.transform.position - pos).normalized * speed * Time.deltaTime;
+                if (TargetObj != null) IsMoving = true;
 
-            transform.rotation = Quaternion.LookRotation(TargetObj.transform.position - pos);
-
-            plus.y = 0f;
-            transform.position = pos + plus;
-
-
-            Collider[] hitColliders = Physics.OverlapSphere(pos, 25.0f);
-
-            foreach (var hitCollider in hitColliders)
-            {
-                TargetObj = null;
-                Animator.SetBool("Punch", true);
-                count = 0;
-                IsAttack = true;
+                //Debug.Log(TargetObj.name +""+ TargetObj.transform.position);
             }
         }
     }
